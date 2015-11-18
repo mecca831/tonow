1d3329dd7b68053395fc15c628e48b956b8693bb
Miguel Hervas <miguelhervas@mikiquantum.local>
2015-11-17 21:19:26 -0800

    Draft login v0

---
+++ todo/www/js/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('login', {
+    url: '/login',
+    templateUrl: 'login.html'
+  })
+  .state('home', {
+    url: '/home',
+    templateUrl: 'home.html',
+    resolve: {
+
+    }
+  })
+
+  $urlRouterProvider.otherwise("/login");
+})
+
+.controller('TodoCtrl', function($scope, $ionicSideMenuDelegate) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+
+  $scope.showHome = function() {
+      $scope.hello = "Hola que tal?"
+  };
+
+  $scope.toggleProjects = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
