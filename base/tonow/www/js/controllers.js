7bfd86b1112908fa1140875e9e8140b9f4515bc3
Miguel Hervas <miguelhervas@mikiquantum.local>
2015-11-22 22:29:17 -0800

    Added slider box for daily cards

--- base/tonow/www/js/controllers.js
+++ base/tonow/www/js/controllers.js
@@ -1,6 +1,30 @@
 angular.module('tonow.controllers', ['ionic'])
 
-.controller('OverviewCtrl', function($scope){
+.controller('OverviewCtrl', function($scope, $timeout) {
   console.log('OverviewCtrl Executed');
-  $scope.variable = "I am the overview!"
+  $scope.hide = false;
+  $scope.variable = "Today";
+  $scope.tasks = ["Clean bathroom", "Make bread", "Something w/ someone"];
+  $scope.cards = [
+    {
+      "day": "Today",
+      "title": "This is today",
+      "tasks": ["Clean bathroom", "Make bread", "Something w/ someone"]
+    },
+    {
+      "day": "Tomorrow",
+      "title": "This is tomorrow",
+      "tasks": ["Buy car wipes", "Shower"]
+    }
+  ];
+
+  // $scope.go = function(direction) {
+  //       $scope.hide = true;
+  //       $scope.past = (direction > 0);
+  //
+  //       $timeout(function() {
+  //         $scope.variable = "Today "+direction;
+  //         $scope.hide = false;
+  //       }, 150);
+  // }
 })
