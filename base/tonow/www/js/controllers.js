6b885d5e8a1cfe836934a91abcbe3beaf9e88c6f
Miguel Hervas <miguelhervas@mikiquantum.local>
2015-12-20 19:11:46 -0800

    list service

--- base/tonow/www/js/controllers.js
+++ base/tonow/www/js/controllers.js
@@ -1,37 +1,14 @@
-angular.module('tonow.controllers', ['ionic'])
+angular.module('tonow.controllers', ['ionic', 'tonow.services'])
 
-.controller('OverviewCtrl', function($scope, $state) {
+.controller('OverviewCtrl', function($scope, $state, OverviewService) {
   console.log('OverviewCtrl Executed');
   $scope.hide = false;
   $scope.variable = "Today";
   $scope.tasks = ["Clean bathroom", "Make bread", "Something w/ someone"];
-  $scope.cards = [
-    {
-      "day": "Yesterday",
-      "title": "This is yesterday",
-      "tasks": [
-        {"text": "Doing the dishes", "state": 1},
-        {"text": "Shower less", "state": 0}
-      ]
-    },
-    {
-      "day": "Today",
-      "title": "This is today",
-      "tasks": [
-        {"text": "Clean bathroom", "state": 0},
-        {"text": "Make bread", "state": 1},
-        {"text": "Something w/ someone", "state": 0}
-      ]
-    },
-    {
-      "day": "Tomorrow",
-      "title": "This is tomorrow",
-      "tasks": [
-        {"text": "Buy car wipes", "state": 1},
-        {"text": "Shower", "state": 0}
-      ]
-    }
-  ];
+  cards = OverviewService.getNotes()
+  $scope.cards = cards
+  $scope.numCards = cards.length
+
 
   $scope.addTask = function() {
     console.log('addTask');
@@ -52,7 +29,7 @@ angular.module('tonow.controllers', ['ionic'])
 .controller('TaskCtrl', function($scope) {
   console.log('TaskCtrl');
   $scope.tasks = ["Clean bathroom", "Make bread", "Something w/ someone"];
-  
+
 })
 
 .controller('LoginCtrl', function($scope, $state) {
@@ -68,5 +45,5 @@ angular.module('tonow.controllers', ['ionic'])
       $state.go('login');
     }
   };
-  
+
 })
